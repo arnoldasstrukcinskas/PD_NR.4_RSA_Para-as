a1f0abc60c07a8a2107674ab4cbebd0f59e946b7
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2026-05-04 17:05:26 +0300

    feat: :sparkles: Create Checker program project

---
+++ Trecia_programa_tikrinimas/src/main/java/lt/viko/eif/astrukcinskas/trecia_programa_tikrinimas/TreciaProgramaTikrinimasApplication.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.astrukcinskas.trecia_programa_tikrinimas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreciaProgramaTikrinimasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreciaProgramaTikrinimasApplication.class, args);
+    }
+
+}
