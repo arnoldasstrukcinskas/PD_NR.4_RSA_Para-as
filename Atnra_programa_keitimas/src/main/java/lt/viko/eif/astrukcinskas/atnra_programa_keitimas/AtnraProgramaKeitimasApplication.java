46c21b3ae5dee5c961524afdc79393f5281f663f
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2026-05-04 20:17:35 +0300

    feat: :sparkles: Initialize secont applciation

---
+++ Atnra_programa_keitimas/src/main/java/lt/viko/eif/astrukcinskas/atnra_programa_keitimas/AtnraProgramaKeitimasApplication.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.astrukcinskas.atnra_programa_keitimas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtnraProgramaKeitimasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtnraProgramaKeitimasApplication.class, args);
+    }
+
+}
---
+++ Atnra_programa_keitimas/src/test/java/lt/viko/eif/astrukcinskas/atnra_programa_keitimas/AtnraProgramaKeitimasApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.astrukcinskas.atnra_programa_keitimas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtnraProgramaKeitimasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
